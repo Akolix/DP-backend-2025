57853b3e1b6076162ba24563eb4efbc5721e61c7
ian <iandonker12@hotmail.com>
2025-10-29 03:25:06 +0100

    cors render front-end

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,7 +1,7 @@
 export const corsOptions = {
     origin: process.env.CORS_ORIGIN
         ? process.env.CORS_ORIGIN.split(',')
-        : ['http://localhost:4200'],
+        : ['http://localhost:4200','https://dp-backend-2025.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
