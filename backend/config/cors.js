a72e6a58f75dff7abe75b6c5cd29710b24ee5ee6
ian <iandonker12@hotmail.com>
2025-10-29 13:17:00 +0100

    Cors change

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,7 +1,7 @@
 export const corsOptions = {
     origin: process.env.CORS_ORIGIN
         ? process.env.CORS_ORIGIN.split(',')
-        : ['http://localhost:4200', 'https://dp-backend-2025-rjhb.onrender.com'],
+        : ['http://localhost:4200', 'https://dp-backend-2025.onrender.com' , 'https://dp-backend-2025-rjhb.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
